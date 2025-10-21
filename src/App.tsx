ff7f6f7d8b891c9825a41a96c761d5e43a6c5ea8
naj-n <najlanazar123@gmail.com>
2025-10-21 11:56:41 +0100

    Remove comment regarding custom routes
    Removed comment about custom routes in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ export default function App() {
 
             <Routes>
               <Route path="/" element={<Index />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
